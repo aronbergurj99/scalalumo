56a751d1d464f74d55690f0dea5fc484be65a48c
Aron Bergur Johannsson <aronbergurj@outlook.com>
2025-01-15 19:46:43 +0000

    Initial commit

---
+++ scalalumo/src/Integration.scala
@@ -0,0 +1,10 @@
+package scalalumo
+
+object Intergration {
+  import scalalumo.*
+  
+
+  def main(args: Array[String]): Unit = 
+    val myString: String = lumo.bg_base.bg_transparent.md(lumobps.flex_col.flex_col)
+    println(myString)
+}
